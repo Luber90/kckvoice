10db69173f732cb8f9ff729ef7914a872a8580f6
Luber90 <luberon90@gmail.com>
2021-01-19 21:49:44 +0100

    dlajura

--- inf140801_inf141185.py
+++ inf140801_inf141185.py
@@ -1,6 +1,23 @@
 import sys
+import scipy.io.wavfile
+from scipy import *
+import matplotlib.pyplot as plt
 
+def data(name):
+    samp, signal = scipy.io.wavfile.read('trainall/'+name)
+    if(len(signal[0])==2):
+        signal = [i[0] for i in signal]
+    time = range(0, len(signal))
+    time = [i/samp for i in time]
+    return [time, signal]
+
+def secToIndex(freq, t):
+    return int(freq*t)
 
 
 if __name__ == "__main__":
+    t, s = data('001_K.wav')
+    plt.plot(t, s)
+    plt.show()
+    print(signal[100])
     print(sys.argv[1])
