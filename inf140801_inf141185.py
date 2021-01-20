f4aec6bf92d99948935d3d2ceb1684d618316a1f
Luber90 <luberon90@gmail.com>
2021-01-20 14:24:06 +0100

    koniec

--- inf140801_inf141185.py
+++ inf140801_inf141185.py
@@ -9,7 +9,7 @@ import warnings
 warnings.simplefilter("ignore")
 
 def data(name):
-    samp, signal = scipy.io.wavfile.read('trainall/'+name)
+    samp, signal = scipy.io.wavfile.read(name)
     if(len(signal.shape)==2):
         signal = [i[0] for i in signal]
     time = range(0, len(signal))
@@ -33,7 +33,6 @@ def cutt(t, s, duration, w):
 def program(name):
     t, s, w = data(name)
     time = len(s)/w
-    
     signal = cutt(t, s, time, w)
     signalK = signal*kaiser(len(signal), 100)
 
@@ -45,46 +44,22 @@ def program(name):
     freqsK = [i/len(signalK1)*w for i in freqsK]
     signalK1[:where(array(freqsK)>70)[0][0]] = 0
 
-    fig, ax = plt.subplots()
     hpik = copy(signalK1)
     for i in range(2, 6):
         d = decimate(signalK1, int(i))
         hpik[:len(d)] *= d
     if(freqsK[where(hpik==max(hpik))[0][0]] > 175):
-        print(name+' K', 'K'==name[-5], freqsK[where(hpik==max(hpik))[0][0]])
-        return 'K'==name[-5]
+        print('K')
     else:
-        print(name+' M', 'M'==name[-5], freqsK[where(hpik==max(hpik))[0][0]])
-        return 'M'==name[-5]
+        print('M')
     
     
 
 if __name__ == "__main__":
-    #program('037_K.wav')
-    ile = 0
-    for i in range(1,92):
-        try:
-            if i < 10:
-                t0 = time.time()
-                if program('00{}_K.wav'.format(i)):
-                    print("czas: ", time.time()-t0)
-                    ile += 1     
-            else:
-                t0 = time.time()
-                if program('0{}_K.wav'.format(i)):
-                    print("czas: ", time.time()-t0)
-                    ile += 1   
-        except:
-            if i < 10:
-                t0 = time.time()
-                if program('00{}_M.wav'.format(i)):
-                    print("czas: ", time.time()-t0)
-                    ile += 1   
-            else:
-                t0 = time.time()
-                if program('0{}_M.wav'.format(i)):
-                    print("czas: ", time.time()-t0)
-                    ile += 1
-    print(ile/91)
+    a = time.time()
+    program(sys.argv[1])
+    print(time.time()-a)
+
+    
     
         
